e24d9cef00c4a0a2a2c67f8a6445693816ac43e2
abdrakhmandim <abdrakhmandim@gmail.com>
2026-02-28 15:50:02 +0500

    Add files via upload

--- request.py
+++ request.py
@@ -4,13 +4,13 @@ import json
 url = "https://api.openai.com/v1/chat/completions"
 headers = {
     "Content-type": "application/json",
-    "Authorization": "Bearer API Key"
+    "Authorization": "Bearer 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"
 }
 
 data = {
     "model": "gpt-4o-mini",
     "messages": [
-        {"role": "user", "content": "что такое n8n и для чего она нужна"},
+        {"role": "user", "content": "что такое n8n"},
     ]
 }
 
